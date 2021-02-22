9634ed073988a95811339b20c87cd6c6ae5ab226
Hector <6560172+hector-codes@users.noreply.github.com>
2021-02-22 21:54:09 +0330

    Add json mime type support

--- includes/hooks.php
+++ includes/hooks.php
@@ -1,3 +1,10 @@
 <?php
 
 namespace Loop_Events;
+
+add_filter( 'mime_types', 'loop_events_mime_types' );
+
+function loop_events_mime_types( $mime_types ) {
+    $mime_types['json'] = 'application/json';
+    return $mime_types;
+}
