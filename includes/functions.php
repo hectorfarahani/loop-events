1cb8114be112e072cdd9420453aac3879aba19e9
Hector <6560172+hector-codes@users.noreply.github.com>
2021-02-22 21:22:32 +0330

    Rename file

--- includes/functions.php
+++ includes/functions.php
@@ -10,3 +10,10 @@ function loop_events_update_option( $option, $new_value ) {
 	$config[ $option ] = $new_value;
 	return update_option( 'loop_events_config', $config );
 }
+
+// Wrap ACF Related functions to avoid multiple checks for ACF existence.
+function loop_events_get_field( $field, $default ) {
+	// Check for ACF, return default if ACF is not exists or field has no value yet.
+	// Get field from ACF functions.
+	// Return Field or default.
+}
