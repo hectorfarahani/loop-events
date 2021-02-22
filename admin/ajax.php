d6630145990edf2d8cc2ced0e973eeddd0e00d14
Hector <6560172+hector-codes@users.noreply.github.com>
2021-02-22 21:58:37 +0330

    Add js for uploading json file

--- admin/ajax.php
+++ admin/ajax.php
@@ -1,10 +1,10 @@
 <?php
 
-add_action( 'wp_ajax_loop_events_save_settings', 'loop_events_save_settings' );
+add_action( 'wp_ajax_loop_events_settings', 'loop_events_settings' );
 
-function loop_events_save_settings() {
+function loop_events_settings() {
 
-	check_ajax_referer( 'loop_events_save_settings', 'nonce' );
+	check_ajax_referer( 'loop_events_settings', 'nonce' );
 
 	$post_type = sanitize_text_field( $_POST['option'] );
 	$value     = sanitize_text_field( $_POST['value'] );
--- admin/assets/src/js/admin.js
+++ admin/assets/src/js/admin.js
@@ -1,3 +1,45 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener('DOMContentLoaded', function (event) {
 
+  const $importBtn = document.getElementById('loop-events-json-import');
+  const nonce = document.getElementById('_wpnonce');
+
+  $importBtn.addEventListener('click', function (e) {
+    const jsonData = document.getElementById('loop-events-json').files[0];
+    const formData = new formData();
+
+    const spinner = showLoader();
+
+    e.target.insertAdjacentElement('afterend', spinner);
+
+    formData.append('action', 'loop_events_settings');
+    formData.append('nonce', nonce.value);
+    formData.append('events', jsonData);
+
+    fetch(ajaxurl, {
+      method: 'POST',
+      'body': formData
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        e.target.parentNode.removeChild(spinner);
+        const badge = showResults(res.success, res.data.results);
+        e.target.insertAdjacentElement('afterend', badge);
+        setTimeout(function () {
+          e.target.parentNode.removeChild(badge);
+        }, 500);
+      });
+  });
+
+  function showLoader() {
+    const loader = document.createElement('div');
+    loader.className = 'loop-event-loader';
+    return loader;
+  }
+
+  function showResults(status, text) {
+    const badge = document.createElement('div');
+    badge.className = status ? 'loop-event-badge-success' : 'loop-event-badge-error';
+    badge.textContent = text;
+    return badge;
+  }
 });
