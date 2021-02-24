d4b463cc5e3a73c98281ca854a2d8ea27506a4ca
Hector <6560172+hector-codes@users.noreply.github.com>
2021-02-24 21:46:46 +0330

    Add helper functions and load templates

--- includes/functions.php
+++ includes/functions.php
@@ -16,10 +16,54 @@ function loop_events_is_acf_active() {
 }
 
 // Wrap ACF Related functions to avoid multiple checks for ACF existence and possible fatal errors in front-end.
-function loop_events_get_field( $field, $default ) {
+function loop_events_get_field( $field, $default = '' ) {
 	if ( ! loop_events_is_acf_active() ) {
 		return $default;
 	}
 
 	return get_field( $field );
+}
+
+function loop_events_show_map_link() {
+	$map_data = loop_events_get_field( 'map_location' );
+	if ( $map_data ) {
+		$latitude  = $map_data['center_lat'];
+		$longitude = $map_data['center_lng'];
+		$map_link  = "https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}";
+		?>
+		<strong><?php esc_html_e( 'Location: ', 'loop-events' ); ?></strong>
+		<a href="<?php echo esc_attr( $map_link ); ?>">
+			<?php esc_html_e( 'View on the map', 'loop-events' ); ?>
+		</a>
+		<?php
+	}
+}
+
+function loop_events_show_organizer_email() {
+	$email = loop_events_get_field( 'organizer_email' );
+	if ( $email ) :
+		?>
+		<strong><?php esc_html_e( 'Organizer Email: ', 'loop-events' ); ?></strong>
+		<a href="mailto:<?php echo esc_attr( $email ); ?>">
+			<?php echo esc_html( $email ); ?>
+		</a>	
+		<?php
+	endif;
+}
+
+function loop_events_show_text_field( $label, $field_key ) {
+	?>
+		<strong><?php echo $label . ': '; ?></strong>
+		<?php echo loop_events_get_field( $field_key ); ?>
+	<?php
+}
+
+function loop_events_show_time() {
+	$date_time = loop_events_get_field( 'date_and_time' );
+	if ( $date_time ) {
+		?>
+		<strong><?php esc_html_e( 'Starts in: ', 'loop-events' ); ?></strong>
+		<?php echo human_time_diff( time(), strtotime( $date_time ) ); ?>
+		<?php
+	}
 }
--- includes/hooks.php
+++ includes/hooks.php
@@ -3,6 +3,22 @@
 add_filter( 'mime_types', 'loop_events_mime_types' );
 
 function loop_events_mime_types( $mime_types ) {
-    $mime_types['json'] = 'application/json';
-    return $mime_types;
+	$mime_types['json'] = 'application/json';
+	return $mime_types;
+}
+
+add_filter( 'template_include', 'loop_events_template_loader' );
+
+function loop_events_template_loader( $template ) {
+	if ( is_post_type_archive( 'loop-event' ) ) {
+        // We can let user load templates from theme by checking it using locate template function.
+		return LOOP_EVENTS_PATH . '/front/templates/archive-loop-event.php';
+	}
+
+    if ( is_singular( 'loop-event' ) ) {
+        // We can let user load templates from theme by checking it using locate template function.
+		return LOOP_EVENTS_PATH . '/front/templates/single-loop-event.php';
+    }
+
+	return $template;
 }
