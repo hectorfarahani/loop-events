0e15fa3f794106338b64f75f8e8dc95b79d02c0e
Hector <6560172+hector-codes@users.noreply.github.com>
2021-02-24 21:45:48 +0330

    Add custom template for events

---
+++ front/templates/single-loop-event.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+<?php
+$post_classes   = get_post_class();
+$post_classes[] = 'entry-content';
+?>
+
+<div <?php post_class( 'entry-content' ); ?> >
+	<header class="entry-header alignwide">
+		<h2 class="entry-title">
+			<?php the_title(); ?>
+		</h2>
+	</header>
+		
+	<main>
+		<?php the_content(); ?>
+		<p>
+			<h4><?php esc_html_e( 'Event details', 'loop-events' ); ?></h4>
+		</p>
+		<p>
+			<?php loop_events_show_text_field( __( 'Organizer Name', 'loop-events' ), 'organizer_name' ); ?>
+		</p>
+		<p>
+			<?php loop_events_show_time(); ?>
+		</p>
+		<p>
+			<?php loop_events_show_organizer_email(); ?>
+		</p>
+		<p>
+			<?php loop_events_show_text_field( __( 'Address', 'loop-events' ), 'address' ); ?>
+		</p>
+		<p>
+			<?php loop_events_show_map_link(); ?>
+		</p>
+		<footer>
+			<p>
+				<?php echo get_the_term_list( $post->ID, LOOP_EVENTS_CPT_SLUG . '-tags', __( 'Tags: ' ), ', ' ); ?>
+			</p>
+		</footer>
+	</main>
+</div>
+
+<?php get_footer(); ?>
