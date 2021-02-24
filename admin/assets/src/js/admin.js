3c0d7c0370492c46c5cb4cf5377d581520850096
Hector <6560172+hector-codes@users.noreply.github.com>
2021-02-24 14:02:04 +0330

    Add UX functionalities
    Loading icon, btn disable-inable

--- admin/assets/src/js/admin.js
+++ admin/assets/src/js/admin.js
@@ -4,43 +4,50 @@ document.addEventListener('DOMContentLoaded', function (event) {
   const nonce = document.getElementById('_wpnonce');
 
   $importBtn.addEventListener('click', function (e) {
-    const jsonData = document.getElementById('loop-events-json').files[0];
+    // Disable button to prevent multiple clicks.
+    e.target.setAttribute('disabled', 'disabled');
 
-    if (undefined === jsonData) {
-      return;
-    }
-
-    const formData = new FormData();
-
+    // Show loading icon.
     const spinner = showLoader();
-
     e.target.insertAdjacentElement('afterend', spinner);
 
+    // Prepare request.
+    const jsonData = document.getElementById('loop-events-json').files[0];
+    const formData = new FormData();
     formData.append('action', 'loop_events_settings');
     formData.append('nonce', nonce.value);
     formData.append('events', jsonData);
 
+    // Send request and handle response.
     fetch(ajaxurl, {
       method: 'POST',
       'body': formData
     })
       .then((response) => response.json())
       .then((res) => {
+        // Remove loading icon.
         e.target.parentNode.removeChild(spinner);
+        // Re-enable button.
+        e.target.removeAttribute('disabled');
+        // Show results.
         const badge = showResults(res.success, res.data.results);
         e.target.insertAdjacentElement('afterend', badge);
+        // Hide shown result badge.
         setTimeout(function () {
           e.target.parentNode.removeChild(badge);
         }, 500);
       });
   });
 
+  // Shows loading icon.
   function showLoader() {
     const loader = document.createElement('div');
-    loader.className = 'loop-event-loader';
+    loader.className = 'loop-events-loader';
     return loader;
   }
 
+
+  // Shows result as a badge and message.
   function showResults(status, text) {
     const badge = document.createElement('div');
     badge.className = status ? 'loop-event-badge-success' : 'loop-event-badge-error';
